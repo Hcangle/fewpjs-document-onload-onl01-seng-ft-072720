80a17fb4c56736a4250397243c370a2c1ce87481
Hannah Angle <hannahcangle@gmail.com>
2020-10-30 22:59:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,13 +9,3 @@ function updateDOM() {
   document.getElementById( "text" )
   .innerHTML = "This is really cool!";
 }
-
-document.addEventListener( "DOMContentLoaded", function () {
-  updateDOM()
-} );
-
-
-function updateDOM() {
-  document.getElementById( "text" )
-    .innerHTML = "This is really cool!";
-}
