7d9f3ea6c42e9a5ae6abdcf918e164434dc41e45
Hannah Angle <hannahcangle@gmail.com>
2020-10-30 22:59:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 // Your code goes here
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("The DOM has loaded");
+  updateDOM()
 });
 
 
 function updateDOM() {
   document.getElementById( "text" )
   .innerHTML = "This is really cool!";
+}
+
+document.addEventListener( "DOMContentLoaded", function () {
+  updateDOM()
+} );
+
+
+function updateDOM() {
+  document.getElementById( "text" )
+    .innerHTML = "This is really cool!";
 }
