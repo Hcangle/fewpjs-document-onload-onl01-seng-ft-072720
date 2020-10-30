f2a92f9c5d97dc81879f167b7aeb06dfb2bc47ee
Hannah Angle <hannahcangle@gmail.com>
2020-10-30 22:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
 });
 
+
+function updateDOM() {
+  document.getElementById( "text" )
+  .innerHTML = "This is really cool!";
+}
